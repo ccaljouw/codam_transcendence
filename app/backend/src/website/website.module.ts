dd4d85f177470cd7c0b5d4fc8bfdd770410a2736
ccaljouw <ccaljouw@student.codam.nl>
2024-01-23 12:59:07 +0100

    minor change for swagger annotation

--- app/backend/src/website/website.module.ts
+++ app/backend/src/website/website.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { WebsiteController } from './website.controller';
+import { join } from 'path';
 
 @Module({
   imports: [
