f04e3c9754f026f28b16e950b987fafa7d4d2c72
jaberkro <jaberkro@student.codam.nl>
2024-01-29 17:13:24 +0100

    swagger full height

--- app/frontend/src/pages/swagger.tsx
+++ app/frontend/src/pages/swagger.tsx
@@ -5,7 +5,7 @@ const SwaggerPage: React.FC = () => {
 
   return (
 			<div className="transcendence-Swagger">
-        <iframe src={externalWebsiteUrl} title="API" style={{ width: '100%', height:'100vh' }}/>
+        <iframe src={externalWebsiteUrl} title="API" style={{ width: '100%', height:'100%' }}/>
       </div>
   );
 };
