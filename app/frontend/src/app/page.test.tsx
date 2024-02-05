c77037cfa360abff463ef2dd5cd8f1541049159e
ccaljouw <ccaljouw@student.codam.nl>
2024-02-05 16:15:07 +0100

    config for frontend tests

--- app/frontend/src/app/page.test.tsx
+++ app/frontend/src/app/page.test.tsx
@@ -1,7 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Page from "./page";
 
-it("App Router: Works with Server Components", () => {
-  render(<Page />);
-  expect(screen.getByRole("heading")).toHaveTextContent("Home page");
+describe('Page Component', () => {
+  it('renders home page with welcome text', () => {
+    render(<Page />);
+    expect(screen.getByText('Home page')).toBeInTheDocument();
+    expect(screen.getByText('Here you can see the welcome text, leaderboard and users. Chat on the bottom')).toBeInTheDocument();
+  });
 });
