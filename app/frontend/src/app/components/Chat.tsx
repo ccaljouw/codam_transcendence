1ee22a9e318aba39869ce98bd4e8e1225c2c984f
jaberkro <jaberkro@student.codam.nl>
2024-02-16 14:22:45 +0100

    long placeholder text to test scrolling added

--- app/frontend/src/app/components/Chat.tsx
+++ app/frontend/src/app/components/Chat.tsx
@@ -1,7 +1,24 @@
 export default function Chat() {
 	return (
 		<>
+		<div className="col">
+
 			<h1>Start a new chat</h1>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+			<p>User1<br/>User2<br/>Bobbie<br/></p>
+		</div>
 		</>
 	);
 }
