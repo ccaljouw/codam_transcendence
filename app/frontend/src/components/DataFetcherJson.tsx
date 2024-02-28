3dffc3cff6637c74035d32812b5ad769d6f76e22
OudeMacVanHannah <oudemacvanhannah@MacBook-Pro-van-OudeMacVanHannah.local>
2024-02-28 17:57:36 +0100

    generic data fetcher

---
+++ app/frontend/src/components/DataFetcherJson.tsx
@@ -0,0 +1,14 @@
+export default async function DataFetcherJson(
+    {url} : {url:string}
+) {
+	try {
+		const response: Response = await fetch(url);
+		if (!response.ok) // todo: check if needed here
+			console.log('Error in DataFetcherJson: response not ok')
+		const result = await response.json();
+		return (result);
+	} catch (error) {
+		console.error('Error in DataFetcherJson:', error);
+		return (error);
+	}
+}
