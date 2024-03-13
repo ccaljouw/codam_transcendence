a3fe11e72af8cf14f8c35d88101add4c1ed4ab32
jaberkro <jaberkro@student.codam.nl>
2024-03-13 11:14:30 +0100

    useFetch not needed anymore, info taken from context

--- app/frontend/src/app/profile/components/LoginSettings.tsx
+++ app/frontend/src/app/profile/components/LoginSettings.tsx
@@ -1,36 +1,21 @@
 "use client";
-import { useEffect } from 'react';
-import { UserProfileDto } from '../../../../../backend/src/users/dto/user-profile.dto';
+import { useContext } from 'react';
 import DataField from "../../../components/DataField";
-import useFetch from 'src/components/useFetch';
-import { constants } from 'src/globals/constants.globalvar';
+import { TranscendenceContext } from 'src/globals/contextprovider.globalvar';
 
 export default function LoginSettings(): JSX.Element {
-	const { data: user, isLoading, error, fetcher } = useFetch<null, UserProfileDto>();
-
-	useEffect(() => {
-		fetchUser();
-	}, []);
-
-	const fetchUser = async () => {
-		const userId = sessionStorage.getItem('userId'); // todo: change to token
-		await fetcher({url: constants.API_SINGLE_USER + userId});
-	}
+	const {currentUser} = useContext(TranscendenceContext);
 
 	return (
 		<>
 			<h1>User information</h1>
-			{isLoading && <p>Loading...</p>}
-			{error && <p>Error: {error.message}</p>}
-			{user && (<>
-				<p>From database:</p>
-				<DataField name="Login name" data={user?.loginName} />
-				<DataField name="First name" data={user?.firstName} />
-				<DataField name="Last name" data={user?.lastName} />
-				<p>
-					Button to Enable two-factor authentication, link to change password
-				</p>
-			</>)}
+			<p>From context:</p>
+			<DataField name="Login name" data={currentUser.loginName} />
+			<DataField name="First name" data={currentUser.firstName} />
+			<DataField name="Last name" data={currentUser.lastName} />
+			<p>
+				Button to Enable two-factor authentication, link to change password
+			</p>
 		</>
 	);
 }
