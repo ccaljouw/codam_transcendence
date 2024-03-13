f020067346e79c4fc5829fd91a8b77ddb82f7ad1
jaberkro <jaberkro@student.codam.nl>
2024-03-13 11:17:57 +0100

    removed newline

--- app/frontend/src/app/components/Welcome.tsx
+++ app/frontend/src/app/components/Welcome.tsx
@@ -3,34 +3,6 @@ import { TranscendenceContext } from "src/globals/contextprovider.globalvar.tsx"
 
 export default function Welcome() : JSX.Element {
 	const {currentUser} = useContext(TranscendenceContext);
-
-	// useEffect(() => {
-	// 	if (typeof window !== 'undefined' && sessionStorage && sessionStorage.getItem != null) {
-	// 		const userNameFromSession = sessionStorage.getItem('userName');
-	// 		const userIdFromSession = sessionStorage.getItem('userId');
-	// 		if (userNameFromSession && userNameFromSession != '')
-	// 		{
-	// 			setCurrentUserName(userNameFromSession);
-	// 		}
-	// 		if (userIdFromSession && userIdFromSession != '')
-	// 		{
-	// 			setCurrentUserId(parseInt(userIdFromSession));
-	// 			if ((!userNameFromSession || userNameFromSession == '') && currentUserId != 0)
-	// 				fetchUserName(currentUserId.toString());
-	// 		}
-	// 	}
-	// }, [currentUserName, someUserUpdatedTheirStatus]);
-
-	// const fetchUserName = async (id: string | null) =>
-	// {
-	// 	console.log(`fetching user for id [${id}]`);
-	// 	if (!id || id == '' || id == '0')
-	// 		return ;	
-	// 	const response = await fetch(constants.BACKEND_ADRESS_FOR_WEBSOCKET + 'users/' + id);
-	// 	const responseJSON : UserProfileDto = await response.json();
-	// 	const userNameFromDb = responseJSON.userName;
-	// 	setCurrentUserName(userNameFromDb);
-	// }
 	
 	return (
 		<>
@@ -39,5 +11,3 @@ export default function Welcome() : JSX.Element {
         </>
 	);
 }
-
-//todo: JMA: use generic data fetcher. Use userName instead of loginName
