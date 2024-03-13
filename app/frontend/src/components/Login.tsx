a30f85f287e790d825d22e6c46b18f67771c610e
jaberkro <jaberkro@student.codam.nl>
2024-03-13 15:16:40 +0100

    unused useState removed

--- app/frontend/src/components/Login.tsx
+++ app/frontend/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import ChooseUser from 'src/app/sign-up/components/ChooseUser';
 import Seed from 'src/app/test/components/Seed';
 import { constants } from '../globals/constants.globalvar'
