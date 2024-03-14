f3ce5880b4e62335504c0b2140f609246a7ea607
jaberkro <jaberkro@student.codam.nl>
2024-03-14 18:13:57 +0100

    removed space

--- app/frontend/src/app/layout.tsx
+++ app/frontend/src/app/layout.tsx
@@ -5,7 +5,7 @@ import '../styles/styleSimpleGrid.css';
 import { ContextProvider } from '../globals/contextprovider.globalvar.tsx';
 import BootstrapClient from './components/BootstrapClient.tsx';
 
-export default async function  RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }){
 	console.log("root rerender");
     return (
 		<html lang="en">
