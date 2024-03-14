63dead66b5eb2e3201ce6d452d9b29d4219e338e
jaberkro <jaberkro@student.codam.nl>
2024-03-14 17:11:03 +0100

    Test component added for all, frontend and backend test pages

--- app/frontend/src/app/test/backend/page.tsx
+++ app/frontend/src/app/test/backend/page.tsx
@@ -1,41 +1,10 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import IframeHolder from '../components/IframeHolder';
+import Test from '../components/Test';
 
-interface DataFormat {
-  msg: string;
-}
-
-export default function BackendTests() {
-  const [data, setData] = useState<DataFormat | null>(null);
-
-  useEffect(() => {
-    //todo: use generic data fetcher
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3001/test/backend');
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    if (data === null) {
-      fetchData();
-    }
-  }, []);
-
+export default function page() : JSX.Element {
   return (
-    <div>
+    <>
       <h1>Backend tests</h1>
-      {data === null ? (
-        <p>Running tests...</p>
-      ) : (
-        <>
-          <IframeHolder url='http://localhost:3001/test/output' title="Backend test output" />
-        </>
-      )}
-    </div>
+      <Test url="http://localhost:3001/test/backend" iframeTitle="Backend tests output" />
+    </>
   );
 }
--- app/frontend/src/app/test/frontend/page.tsx
+++ app/frontend/src/app/test/frontend/page.tsx
@@ -1,41 +1,10 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import IframeHolder from '../components/IframeHolder';
+import Test from '../components/Test';
 
-interface DataFormat {
-  msg: string;
-}
-
-export default function FrontendTests() {
-  const [data, setData] = useState<DataFormat | null>(null);
-
-  useEffect(() => {
-    //todo: use generic data fetcher
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3001/test/frontend');
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    if (data === null) {
-      fetchData();
-    }
-  }, []);
-
+export default function page() : JSX.Element {
   return (
-    <div>
+    <>
       <h1>Frontend tests</h1>
-      {data === null ? (
-        <p>Running tests...</p>
-      ) : (
-        <>
-          <IframeHolder url='http://localhost:3001/test/output' title="Frontend test output" />
-        </>
-      )}
-    </div>
+      <Test url="http://localhost:3001/test/frontend" iframeTitle="Frontend tests output" />
+    </>
   );
 }
