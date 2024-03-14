8245cd5117ce441f8958c0028eb147ccbb19465f
jaberkro <jaberkro@student.codam.nl>
2024-03-14 16:55:45 +0100

    bootstrapClient re-added after magical disappearance

--- app/frontend/src/app/layout.tsx
+++ app/frontend/src/app/layout.tsx
@@ -3,15 +3,17 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/stylesheet.css';
 import '../styles/styleSimpleGrid.css';
 import { ContextProvider } from '../globals/contextprovider.globalvar.tsx';
+import BootstrapClient from './components/BootstrapClient.tsx';
 
 export default async function  RootLayout({ children }: { children: React.ReactNode }) {
 	console.log("root rerender");
     return (
 		<html lang="en">
 			<body>
-			<div className="root-layout">
-			<ContextProvider>{children}</ContextProvider>
-			</div>
+				<div className="root-layout">
+					<ContextProvider>{children}</ContextProvider>
+				</div>
+				<BootstrapClient />
 			</body>
 		</html>
     );
