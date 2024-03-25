9af96969653492bf002ae2133e40fbea99286e4f
jaberkro <jaberkro@student.codam.nl>
2024-03-25 16:43:00 +0100

    example implementation

--- app/frontend/src/globals/layoutComponents/Font.tsx
+++ app/frontend/src/globals/layoutComponents/Font.tsx
@@ -2,7 +2,7 @@ import { Comic_Neue, Bangers } from '@next/font/google';
 import Pop_Art from '@next/font/local';
 
 export const bangers = Bangers({weight: "400", preload: false, variable: '--my-font-bangers'});
-export const comic_neue = Comic_Neue({weight: "300", preload: false, variable: '--my-font-comic_neue'});
+export const comic_neue = Comic_Neue({weight: "400", preload: false, variable: '--my-font-comic_neue'});
 export const pop_art = Pop_Art({
 	src: "../../app/public/fonts/PopArt-Regular.ttf",
 	preload: true,
--- app/frontend/src/globals/layoutComponents/MenuBar.tsx
+++ app/frontend/src/globals/layoutComponents/MenuBar.tsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { TranscendenceContext } from '@ft_global/contextprovider.globalvar';
+import { FontBangers, FontPopArt } from './Font';
 
 function MenuLink({href, title}:{href:string, title:string}){
 	const pathname = usePathname();
@@ -19,38 +20,42 @@ export default function MenuBar(): JSX.Element {
 	return (
 		<>
 			<nav className="navbar navbar-expand-md">
-				<a className="navbar-brand" href="/">PONG</a>
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
-					<div className="navbar-nav">
-						{currentUser.id && <>
-							<MenuLink href="/" title="Home" />
-							<MenuLink href="/profile" title="Profile" />
-							<MenuLink href="/play" title="Play" />
-							<MenuLink href="/logout" title="Logout" />
-						</>}
+				<FontBangers>
+					<a className="navbar-brand" href="/">PONG</a>
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+				</FontBangers>
+				<FontPopArt>
+					<div className="collapse navbar-collapse" id="navbarNav">
+						<div className="navbar-nav">
+							{currentUser.id && <>
+								<MenuLink href="/" title="Home" />
+								<MenuLink href="/profile" title="Profile" />
+								<MenuLink href="/play" title="Play" />
+								<MenuLink href="/logout" title="Logout" />
+							</>}
+						</div>
+						<div className={"navbar-nav"}>
+							{/* todo: limit access on the pages itself as well */}
+							{(currentUser.loginName == 'ccaljouw' || currentUser.loginName == 'jaberkro' || currentUser.loginName == 'avan-and' || currentUser.loginName == 'cwesseli') && <>
+								<li className="nav-item dropdown">
+									<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+										Developer options
+									</a>
+									<ul className="dropdown-menu">
+										<li><Link className="dropdown-item" href="/dev/swagger">Swagger</Link></li>
+										<li><hr className="dropdown-divider"/></li>
+										<li><Link className="dropdown-item" href="/dev/test/all">Test all</Link></li>
+										<li><Link className="dropdown-item" href="/dev/test/frontend">Test frontend</Link></li>
+										<li><Link className="dropdown-item" href="/dev/test/backend">Test backend</Link></li>
+										<li><Link className="dropdown-item" href="/dev/test/coverage">Test coverage</Link></li>
+									</ul>
+								</li>
+							</>}
+						</div>
 					</div>
-					<div className={"navbar-nav"}>
-						{/* todo: limit access on the pages itself as well */}
-						{(currentUser.loginName == 'ccaljouw' || currentUser.loginName == 'jaberkro' || currentUser.loginName == 'avan-and' || currentUser.loginName == 'cwesseli') && <>
-							<li className="nav-item dropdown">
-								<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-									Developer options
-								</a>
-								<ul className="dropdown-menu">
-									<li><Link className="dropdown-item" href="/dev/swagger">Swagger</Link></li>
-									<li><hr className="dropdown-divider"/></li>
-									<li><Link className="dropdown-item" href="/dev/test/all">Test all</Link></li>
-									<li><Link className="dropdown-item" href="/dev/test/frontend">Test frontend</Link></li>
-									<li><Link className="dropdown-item" href="/dev/test/backend">Test backend</Link></li>
-									<li><Link className="dropdown-item" href="/dev/test/coverage">Test coverage</Link></li>
-								</ul>
-							</li>
-						</>}
-					</div>
-				</div>
+				</FontPopArt>
 			</nav>
 		</>
 	);
