091a42e53c67833528af09d91cde09f8270df534
jaberkro <jaberkro@student.codam.nl>
2024-04-04 20:17:40 +0200

    validation already done at userInfo component

--- app/frontend/src/globals/functionComponents/form/validation/InvalidUserName.tsx
+++
@@ -1,8 +0,0 @@
-export default function InvalidUserName(name: string) : boolean {
-    const minLength: number = 3;
-    const maxLength: number = 30; //todo: consider to add these values to global constants
-
-    if (name.length < minLength || name.length > maxLength)
-        return (true);
-    return (false);
-}
