8c36b9e975f86085507ef99919cfa97900140279
WinglessOracle <carlo@live.nl>
2024-04-08 21:31:35 +0200

    elint modifications to the game controller

--- app/backend/src/game/game.controller.ts
+++ app/backend/src/game/game.controller.ts
@@ -1,5 +1,18 @@
-import { Controller, ParseIntPipe, Param, Get, Delete, Patch, Body } from '@nestjs/common';
-import { ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  ParseIntPipe,
+  Param,
+  Get,
+  Delete,
+  Patch,
+  Body,
+} from '@nestjs/common';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { UpdateGameDto } from 'dto/game/update-game.dto';
 import { GameService } from './game.service';
 import { UpdateGameStateDto } from 'dto/game/update-game-state.dto';
@@ -10,8 +23,13 @@ export class GameController {
   constructor(private readonly gameService: GameService) {}
 
   @Get('getGame/:userId')
-  @ApiOperation({ summary: 'Checks if there is a player waiting and if so returns new game id'})
-  async getGame(@Param('userId', ParseIntPipe) userId: number) : Promise<UpdateGameDto> {
+  @ApiOperation({
+    summary:
+      'Checks if there is a player waiting and if so returns new game id',
+  })
+  async getGame(
+    @Param('userId', ParseIntPipe) userId: number,
+  ): Promise<UpdateGameDto> {
     const game = await this.gameService.getGame(userId);
     console.log(game.GameUsers[0]);
     console.log(game.GameUsers[1]);
@@ -19,36 +37,38 @@ export class GameController {
   }
 
   @Get('all')
-  @ApiOperation({ summary: 'Returns all games currently in the database'})
+  @ApiOperation({ summary: 'Returns all games currently in the database' })
   @ApiOkResponse({ type: [UpdateGameDto] })
-  @ApiNotFoundResponse({ description: "No games in the database" })
-
-  findAll() : Promise<UpdateGameDto[]> {
+  @ApiNotFoundResponse({ description: `No games in the database` })
+  findAll(): Promise<UpdateGameDto[]> {
     return this.gameService.findAll();
   }
 
   @Get(':id')
-  @ApiOperation({ summary: 'Returns game with specified id'})
+  @ApiOperation({ summary: 'Returns game with specified id' })
   @ApiOkResponse({ type: UpdateGameDto })
   @ApiNotFoundResponse({ description: 'Game with #${id} does not exist' })
-
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.gameService.findOne(id);
   }
 
   @Patch(':id')
-  @ApiOperation({ summary: 'Updates user with specified id'})
+  @ApiOperation({ summary: 'Updates user with specified id' })
   @ApiOkResponse({ type: UpdateGameStateDto })
-
-  update(@Param('id', ParseIntPipe) id: number, @Body() updateGameStateDto: UpdateGameStateDto) : Promise<number> {
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateGameStateDto: UpdateGameStateDto,
+  ): Promise<boolean> {
     return this.gameService.update(updateGameStateDto);
   }
 
   @Delete(':id')
-  @ApiOperation({ summary: 'Deletes game with specified id'})
-  @ApiOkResponse({ description: 'Game successfully deleted', type: UpdateGameDto })
-
-  remove(@Param('id', ParseIntPipe) id: number) : Promise<UpdateGameDto> {
+  @ApiOperation({ summary: 'Deletes game with specified id' })
+  @ApiOkResponse({
+    description: 'Game successfully deleted',
+    type: UpdateGameDto,
+  })
+  remove(@Param('id', ParseIntPipe) id: number): Promise<UpdateGameDto> {
     return this.gameService.remove(id);
   }
 }
