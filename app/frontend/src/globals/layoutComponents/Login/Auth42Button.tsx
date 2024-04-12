6b05d8dfee06715579fc8d59a696806af58ba99f
jaberkro <jaberkro@student.codam.nl>
2024-04-12 13:18:47 +0200

    Link made more readable

--- app/frontend/src/globals/layoutComponents/Login/Auth42Button.tsx
+++ app/frontend/src/globals/layoutComponents/Login/Auth42Button.tsx
@@ -1,19 +1,19 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { constants } from '@ft_global/constants.globalvar';
-// import { env } from '../../../../../backend/.env';
 
 export default function Auth42Button() : JSX.Element {
-    const pathname = usePathname();
-    console.log(encodeURIComponent(pathname));
-    // console.log(`${constants.API_LOGIN_42}&redirect_uri=${encodeURIComponent(constants.FRONTEND_BASEURL + pathname)}&response_type=code`);
+	const pathname = usePathname();
+	{/* todo jma: use env variables to create url and clientId: */}
+	const url = `https://api.intra.42.fr/oauth/authorize`;
+	const clientId = `u-s4t2ud-66c50cf4e54a51062bc5f0c110035ff12e1b0427cc1066c11d6e5c220a2ed1dc`;
+	const redirectUri = encodeURIComponent(constants.FRONTEND_BASEURL + pathname);
 
-    return (
-        <>
-        {/* todo jma: use proper env variables to create url: */}
-            <Link className="btn btn-primary" href={`https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-66c50cf4e54a51062bc5f0c110035ff12e1b0427cc1066c11d6e5c220a2ed1dc&redirect_uri=${encodeURIComponent(constants.FRONTEND_BASEURL + pathname)}&response_type=code`}>Login with 42</Link>
-            {/* <Link className="btn btn-primary" href={`https://api.intra.42.fr/oauth/authorize?client_id=${env.CLIENT_ID}&redirect_uri=${encodeURIComponent(constants.FRONTEND_BASEURL + pathname)}&response_type=code&scope=${env.SCOPE}&state=${env.STATE}`}>Login with 42</Link> */}
-            {/* <Link className="btn btn-primary" href={`${constants.API_LOGIN_42}&redirect_uri=${encodeURIComponent(constants.FRONTEND_BASEURL + pathname)}&response_type=code`}>Login with 42</Link> */}
-        </>
-    );
+	return (
+		<>
+			<Link className="btn btn-primary" href={`${url}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`}>
+				Login with 42
+			</Link>
+		</>
+	);
 }
