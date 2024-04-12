ff62cb43207ce948df9e639ee0e93b4d87a9a46a
jaberkro <jaberkro@student.codam.nl>
2024-04-12 13:06:40 +0200

    remove code query from pathname after logging in

--- app/frontend/src/globals/layoutComponents/Login/CheckAlreadyLoggedIn.tsx
+++ app/frontend/src/globals/layoutComponents/Login/CheckAlreadyLoggedIn.tsx
@@ -1,40 +1,20 @@
 import { useContext, useEffect } from 'react';
-import { useSearchParams } from 'next/navigation';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import { TranscendenceContext } from 'src/globals/contextprovider.globalvar';
 import useFetch from 'src/globals/functionComponents/useFetch';
 import { UserProfileDto } from '@ft_dto/users';
 import { constants } from '@ft_global/constants.globalvar';
 
 export default function CheckAlreadyLoggedIn() {
-    const idFromSession = sessionStorage.getItem('userId');
-	const searchParams = useSearchParams();
-	const code = searchParams.get('code');
 	const {currentUser, setCurrentUser} = useContext(TranscendenceContext);
 	const { data: user, fetcher: userFetcher } = useFetch<null, UserProfileDto>();
+    const idFromSession = sessionStorage.getItem('userId');
+	const code = useSearchParams().get('code');
+	const pathname = usePathname();
+	const router = useRouter();
 
-    const  setLoggedUser = async (user: UserProfileDto) => {
-		console.log("Setting new user with id " + user.id + " in LoginScreen");
-		setCurrentUser(user);
-		sessionStorage.setItem('userId', JSON.stringify(user.id));
-	}
-
-    useEffect(() => {
-		if (user != null)
-		{
-			setLoggedUser(user);
-			return ;
-		}
-	}, [user]);
-
-	const  fetchUser = async (url: string) => {
-		userFetcher({url: url});
-	}
-
-    useEffect(() => {
-		
-	
-
-        if (idFromSession != null)
+	useEffect(() => {
+		if (idFromSession != null)
 		{
 			console.log("User already logged in. Id: " + idFromSession);
 			if (Object.keys(currentUser).length == 0)
@@ -48,8 +28,27 @@ export default function CheckAlreadyLoggedIn() {
 		{
 			console.log("User logged in with auth42. Code: " + code);
 			fetchUser(constants.API_AUTH42 + code);
+			router.push(pathname);
 			return ;
 		}
-    }, []);
-    return (<></>);
+	}, []);
+	
+	useEffect(() => {
+		if (user != null)
+		{
+			setLoggedUser(user);
+			return ;
+		}	
+	}, [user]);	
+	
+	const  fetchUser = async (url: string) => {
+		userFetcher({url: url});
+	}
+	
+    const  setLoggedUser = (user: UserProfileDto) => {
+		console.log("Setting new user with id " + user.id + " in LoginScreen");
+		setCurrentUser(user);
+		sessionStorage.setItem('userId', JSON.stringify(user.id));
+	}
+    return ;
 }
