8372d336efca08b1a7e9310c917243741ded70df
jaberkro <jaberkro@student.codam.nl>
2024-05-13 20:27:53 +0200

    suggestion from Albert followed, when user is currentUser, there won't be a request to the backend

--- app/frontend/src/app/profile/[username]/components/ProfileClient.tsx
+++ app/frontend/src/app/profile/[username]/components/ProfileClient.tsx
@@ -6,7 +6,7 @@ import GameSettings from "./GameSettings.tsx";
 import Blocked from "./Blocked.tsx";
 import { H3 } from '@ft_global/layoutComponents/Font';
 import { usePathname, useRouter } from "next/navigation";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import useFetch from "src/globals/functionComponents/useFetch.tsx";
 import { UserProfileDto } from "@ft_dto/users/user-profile.dto.ts";
 import { constants } from "src/globals/constants.globalvar.tsx";
@@ -14,9 +14,10 @@ import { TranscendenceContext } from "src/globals/contextprovider.globalvar.tsx"
 
 export default function ProfileClient({userName} : {userName : string}) : JSX.Element {
 	const {currentUser} = useContext(TranscendenceContext);
-	const {data: user, isLoading, error, fetcher} = useFetch<null, UserProfileDto>();
+	const {data, isLoading, error, fetcher} = useFetch<null, UserProfileDto>();
 	const pathname = usePathname();
 	const router = useRouter();
+	const [user, setUser] = useState<UserProfileDto | null>(null);
 	
 	useEffect(() => {
 		console.log(pathname);
@@ -27,8 +28,16 @@ export default function ProfileClient({userName} : {userName : string}) : JSX.El
 	}, []);
 
 	useEffect(() => {
-		fetchUser();
+		if(userName == currentUser.userName)
+			setUser(currentUser);
+		else
+			fetchUser();
 	}, [currentUser]);
+
+	useEffect(() => {
+		if(data != null)
+			setUser(data);
+	}, [data]);
 
 	const fetchUser = async () => {
 		await fetcher({url: constants.API_USERS + userName});
@@ -42,7 +51,7 @@ export default function ProfileClient({userName} : {userName : string}) : JSX.El
 			{error != null &&
 				<H3 text={`Oops, it seems that the user ${userName} does not exist...`}/>
 			}
-			{user != null && user.userName != null ? 
+			{(user != null && user.userName != null) ? 
 				<>
 					<div className="white-box">
 						<H3 text={`Profile page of ${user.userName}`} />  
