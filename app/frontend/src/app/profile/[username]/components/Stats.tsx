f4a14d23315a33f62c6dbdbe34ad63e78d7cfc6a
jaberkro <jaberkro@student.codam.nl>
2024-06-07 12:23:38 +0200

    with Carien

--- app/frontend/src/app/profile/[username]/components/Stats.tsx
+++ app/frontend/src/app/profile/[username]/components/Stats.tsx
@@ -5,6 +5,7 @@ import Achievements from "./utils/Achievements";
 
 export default function Stats({user} : {user: UserProfileDto}) : JSX.Element {
     const testAchievements= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+    //todo: Carien: Hier vervangen met api
 
     return (
         <>
