eab794026d958ffd04b6d8bfef51afaca9051bf1
jaberkro <jaberkro@student.codam.nl>
2024-06-27 10:28:50 +0200

    game state only set to aborted when pressing leave button if game is not finished yet

--- app/frontend/src/app/game/page.tsx
+++ app/frontend/src/app/game/page.tsx
@@ -2,8 +2,6 @@ import GameComponent from "./components/Game.tsx";
 
 export default function Page() {
 	return (
-		// <div className="white-box">
-			<GameComponent />
-		// </div>
+		<GameComponent />
 	);
 }
