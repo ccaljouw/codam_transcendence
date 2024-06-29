590708d48bccc1dbc361a1590456c9faa5419bf0
jaberkro <jaberkro@student.codam.nl>
2024-06-30 00:20:07 +0200

    game schalable again

--- app/frontend/src/app/game/components/Game.tsx
+++ app/frontend/src/app/game/components/Game.tsx
@@ -153,22 +153,20 @@ export default function GameComponent() {
 		router.push('/play');
 	}
 
-
-	// return the canvas
 	return (
 		<>
 			{waitingForPlayers ? (
-				<div className="white-box">
+				<div>
 					<p>Waiting for second player to join...</p>
 				</div>
 			) : (
-				<div className={`${styles.game} white-box`}>
-					<div className={`${styles.gameMenu} text-center row`}>
-						<button className="btn btn-dark" onClick={handleClick}>Leave Game</button>
+				<div className={`${styles.game}`}>
+					<div className={`row`}>
+						<button className="btn btn-dark text-center" onClick={handleClick}>Leave Game</button>
 					</div>
 					<canvas ref={canvasRef} tabIndex={0} />
 				</div>
 			)}
 		</>
 	);
-}	
+}
--- app/frontend/src/app/game/page.tsx
+++ app/frontend/src/app/game/page.tsx
@@ -1,7 +1,10 @@
 import GameComponent from "./components/Game.tsx";
+import styles from './styles.module.css';
 
 export default function Page() {
 	return (
-		<GameComponent />
+		<div className={`white-box ${styles.gameShell}`}>
+			<GameComponent />
+		</div>
 	);
 }
