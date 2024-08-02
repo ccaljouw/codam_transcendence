c6fb6066485581da7a5b7d828068ca05f3fea3a8
jaberkro <jaberkro@student.codam.nl>
2024-08-02 14:51:58 +0200

    return type added

--- app/frontend/src/app/game/[inviteId]/page.tsx
+++ app/frontend/src/app/game/[inviteId]/page.tsx
@@ -1,8 +1,9 @@
 import GameComponent from "./components/Game.tsx";
 import styles from './styles.module.css';
 
-export default function Page({params} : {params:{inviteId: number}}) {
-  const inviteIdNum: number = Number(params.inviteId);
+export default function Page({params} : {params: {inviteId: number}}) : JSX.Element {
+	const inviteIdNum: number = Number(params.inviteId);
+
 	return (
 		<div className={`white-box ${styles.gameShell}`}>
 			<GameComponent inviteId={inviteIdNum}/>
