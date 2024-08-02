10ca1ae0ed26e7aa153414c6bc12258bd686310d
jaberkro <jaberkro@student.codam.nl>
2024-08-02 14:55:45 +0200

    variable declaration removed

--- app/frontend/src/app/game/[inviteId]/page.tsx
+++ app/frontend/src/app/game/[inviteId]/page.tsx
@@ -2,11 +2,9 @@ import GameComponent from "./components/Game.tsx";
 import styles from './styles.module.css';
 
 export default function Page({params} : {params: {inviteId: number}}) : JSX.Element {
-	const inviteIdNum: number = Number(params.inviteId);
-
 	return (
 		<div className={`white-box ${styles.gameShell}`}>
-			<GameComponent inviteId={inviteIdNum}/>
+			<GameComponent inviteId={Number(params.inviteId)}/>
 		</div>
 	);
 }
