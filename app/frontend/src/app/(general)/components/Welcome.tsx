f91a20bb52872e9d6a25c155cb52183ebc17727b
jaberkro <jaberkro@student.codam.nl>
2024-09-04 18:41:48 +0200

    update new features described

--- app/frontend/src/app/(general)/components/Welcome.tsx
+++ app/frontend/src/app/(general)/components/Welcome.tsx
@@ -11,15 +11,14 @@ export default function Welcome() : JSX.Element {
             <H3 text={`Welcome ${currentUser.userName},`}/>
             <p>Challenge your friends to play pong and make new ones on the way!</p>
 
-            <H3 text="NEWS 26/08"/>
+            <H3 text="NEWS 04/09"/>
 			<p>Newly implemented on the frontend side:</p>
-			<li>Login ans Signup have their own page. In theory pages don't get loaded if they won't be shown fast now.</li>
-			<li>Layout split over general and authentication layout.</li>
+			<li>Logout bug origin found and understood</li>
+			<li>Change password works correctly</li>
 			<li>Game does not use sessionStorage userId but currentUser.id</li>
-			<li>A lot of files moved from their original location</li>
-
-			<H3 text="BUG"/>
-			<li>When you reload the page after finishing a game you get logged out sometimes. I don't know why this happpens.</li>
+			<li>Min and max values defined</li>
+			<li>Seed done in package.json instead of login component</li>
+			<li>Ladder positions shown on home page and profile page</li>
         </>
 	);
 }
