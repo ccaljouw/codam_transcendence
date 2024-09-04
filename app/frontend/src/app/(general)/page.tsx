513f87f27c573c6766313539230176dcef88e855
jaberkro <jaberkro@student.codam.nl>
2024-09-04 15:05:22 +0200

    ladder added on home page

--- app/frontend/src/app/(general)/page.tsx
+++ app/frontend/src/app/(general)/page.tsx
@@ -1,16 +1,23 @@
 import Welcome from "./components/Welcome.tsx";
-import Leaderboard from './components/Leaderboard.tsx';
+import { H3 } from "src/globals/layoutComponents/Font.tsx";
+import Top10 from "src/globals/layoutComponents/Top10.tsx";
+import { constants } from "src/globals/constants.globalvar.tsx";
 
 export default function Page() : JSX.Element {
 	return (
 		<>
 			<div>
 				<div className="row">
-					<div className="col col-6 white-box">
-						<Welcome />
+					<div className="col col-4 white-box">
+						<Welcome/>
 					</div>
-					<div className="col col-6 white-box">
-						<Leaderboard />
+					<div className="col col-4 white-box">
+						<H3 text="Leaderboard"/>
+						<Top10 url={constants.API_RANK_TOP_10}/>
+					</div>
+					<div className="col col-4 white-box">
+						<H3 text="Ladder"/>
+						<Top10 url={constants.API_LADDER_TOP_10}/>
 					</div>
 				</div>
 			</div>
