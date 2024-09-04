89c774eaa765fec62df1c166705b18a518032b8c
jaberkro <jaberkro@student.codam.nl>
2024-09-04 17:24:28 +0200

    EditableDataField updated

--- app/frontend/src/app/(general)/profile/[username]/components/UserInfo.tsx
+++ app/frontend/src/app/(general)/profile/[username]/components/UserInfo.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FormEvent, useContext, useEffect, useState } from 'react';
+import { FormEvent, useContext, useEffect } from 'react';
 import { TranscendenceContext } from '@ft_global/contextprovider.globalvar';
 import StaticDataField from 'src/app/(general)/profile/[username]/components/utils/StaticDataField';
 import EditableDataField from 'src/app/(general)/profile/[username]/components/utils/EditableDataField';
@@ -8,73 +8,71 @@ import { UpdateUserDto, UserProfileDto } from '@ft_dto/users';
 import { constants } from '@ft_global/constants.globalvar';
 import useFetch from 'src/globals/functionComponents/useFetch';
 import FormToUpdateUserDto from 'src/globals/functionComponents/form/FormToUpdateUserDto';
-import { optionalAttributes } from 'src/app/(general)/profile/[username]/components/utils/EditableDataField';
 import Avatar from 'src/app/(general)/profile/[username]/components/Avatar';
 import TwoFactorAuthentication from './TwoFactorAuthentication';
 import ChangePassword from './ChangePassword';
 import { useRouter } from 'next/navigation';
 
-export default function UserInfo({user, editable} : {user: UserProfileDto, editable: boolean}): JSX.Element {
+export default function UserInfo({user, editable} : {user: UserProfileDto, editable: boolean}) : JSX.Element {
 	const {currentUser, setCurrentUser} = useContext(TranscendenceContext);
 	const {data: updatedUser, isLoading: loadingUser, error: errorUser, fetcher: fetchUser} = useFetch<UpdateUserDto, UserProfileDto >();
-	const usernameAttributes: optionalAttributes={type:"text", name:"userName", required:true, autoComplete:"off", minLength:3, maxLength:30};
-	const firstnameAttributes: optionalAttributes={type:"text", name:"firstName", required:true, autoComplete:"off", minLength:1, maxLength:30};
-	const lastnameAttributes: optionalAttributes={type:"text", name:"lastName", required:true, autoComplete:"off", minLength:1, maxLength:30};
-	const emailAttributes: optionalAttributes={type:"email", name:"email", required:true, autoComplete:"on"};
-	const hashAttributes: optionalAttributes={type:"password", name:"pwd", required:true, autoComplete:"off", minLength:3, maxLength:30};
-  const router = useRouter();
+	const router = useRouter();
 	
 	const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
 		console.log("submitting form field entry");
 		event.preventDefault();
 		await fetchUser({url: constants.API_USERS + currentUser.id, fetchMethod: 'PATCH', payload: FormToUpdateUserDto(event)})
 	}
-  
+	
 	useEffect(() => {
-    if (updatedUser != null)
-      {
-        //TODO: jorien, evaluate this hack and change if needed
-        if (currentUser.userName != updatedUser.userName)
-        {
-            console.log("It seems that the username has been changed");
-            router.push(`/profile/${updatedUser.userName}`);
-        }
-        setCurrentUser(updatedUser);
-        console.log("reload page from userInfo");
-      }
+		if (updatedUser != null)
+			{
+				if (currentUser.userName != updatedUser.userName)
+				{
+					console.log("It seems that the username has been changed");
+					router.push(`/profile/${updatedUser.userName}`);
+				}
+				setCurrentUser(updatedUser);
+				console.log("reload page from userInfo");
+			}
 	}, [updatedUser]);
 
 	return (
 		<>
 			<H3 text="User information"/>
-      {editable == false && <Avatar user={user} editable={false}/>}
-      {editable == true && <Avatar user={user} editable={true}/>}
+			<Avatar user={user} editable={editable}/>
+			<hr></hr>
 			{editable == false && <StaticDataField name="Username" data={user.userName}/>}
 			{editable == true && 
 				<form onSubmit={handleSubmit} acceptCharset='utf-8' className="row">
-					<EditableDataField name="Username" data={user.userName} attributes={usernameAttributes}/>
+					<EditableDataField name="Username" data={user.userName}>
+						<input className="form-control form-control-sm" placeholder={user.userName} type="text" name="userName" required={true} autoComplete="off" minLength={4} maxLength={15}></input>
+					</EditableDataField>
 				</form>
 			}
 			<StaticDataField name="Online" data={user.online} /> 
-
 			{editable == true && user.userName == currentUser.userName && <>
 				<form onSubmit={handleSubmit} acceptCharset='utf-8' className="row">
-					<EditableDataField name="First name" data={user.firstName} attributes={firstnameAttributes}/>
+					<EditableDataField name="First name" data={user.firstName}>
+						<input className="form-control form-control-sm" placeholder={user.firstName} type="text" name="firstName" required={true} autoComplete="off" minLength={1} maxLength={15}></input>
+					</EditableDataField>
 				</form>
 				<form onSubmit={handleSubmit} acceptCharset='utf-8' className="row">
-					<EditableDataField name="Last name" data={user.lastName} attributes={lastnameAttributes}/>
+					<EditableDataField name="Last name" data={user.lastName}>
+						<input className="form-control form-control-sm" placeholder={user.lastName} type="text" name="lastName" required={true} autoComplete="off" minLength={1} maxLength={15}></input>
+					</EditableDataField>
 				</form>
 				<form onSubmit={handleSubmit} acceptCharset='utf-8' className="row">
-					<EditableDataField name="Email" data={user.email} attributes={emailAttributes}/>
+					<EditableDataField name="Email" data={user.email}>
+						<input className="form-control form-control-sm" placeholder={user.email} type="email" name="email" required={true} autoComplete="on"></input>
+					</EditableDataField>
 				</form>
-        <ChangePassword/>
 				<hr></hr>
-        <TwoFactorAuthentication/>
+				<ChangePassword/>
+				<TwoFactorAuthentication/>
 			</>}
 			{loadingUser == true && <p>Updating user...</p>}
 			{errorUser != null && <p>Not possible to update user: {errorUser.message}</p>}
 		</>
 	);
 }
-
-//todo: 2FA, change password
