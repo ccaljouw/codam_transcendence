b19262e33203b91018af74ea2fb9f8914633d49f
ccaljouw <ccaljouw@student.codam.nl>
2024-09-07 21:36:42 +0200

    change location staturpscript backend to enable dev startup

--- app/startup-script-backend.sh
+++ app/startup-script-backend.sh
@@ -7,4 +7,4 @@ COLOR_BLUE=\033[34m
 
 # Run the application
 echo "Starting the application..."
-exec npx prisma studio & npx prisma migrate deploy & npx prisma db seed & npm run start:dev
+exec npx prisma studio & npx prisma migrate deploy & npx prisma db seed & ${1}
