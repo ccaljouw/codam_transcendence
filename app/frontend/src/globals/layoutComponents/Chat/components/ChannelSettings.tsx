fed7e0c4f0483e6031e466c60378cb316eb47e18
post@albertvanandel.nl <you@example.com>
2024-09-12 22:51:09 +0200

    more intuitive text input

--- app/frontend/src/globals/layoutComponents/Chat/components/ChannelSettings.tsx
+++ app/frontend/src/globals/layoutComponents/Chat/components/ChannelSettings.tsx
@@ -14,6 +14,7 @@ export default function ChannelSettings({ room }: { room: FetchChatDto }) {
 	const { data: chatDeleted, error: deleteError, isLoading: deleteIsLoading, fetcher: chatDeleter } = useFetch<Partial<FetchChatDto>, boolean>();
 	const { data: setChatPassword, error: setChatPasswordError, isLoading: setChatPasswordIsLoading, fetcher: chatPasswordSetter } = useFetch<ChatAuthDto, boolean>();
 	const [password, setPassword] = useState<string>("");
+	const [showSuccess, setShowSuccess] = useState<boolean>(false);
 	// const { data: chatPatched, error: patchError, isLoading: patchIsLoading, fetcher: chatPatcher } = useFetch<Partial<UpdateChatDto>, boolean>();
 
 	const submitChannelNameChange = async (event: FormEvent<HTMLFormElement>) => {
@@ -32,7 +33,7 @@ export default function ChannelSettings({ room }: { room: FetchChatDto }) {
 	useEffect(() => {
 		if (setChatPassword) {
 			console.log("Chat password set");
-			setChatPassword
+			setShowSuccess(true);
 		}
 	}, [setChatPassword]);
 
@@ -68,12 +69,12 @@ export default function ChannelSettings({ room }: { room: FetchChatDto }) {
 			{((chatPatched && chatPatched.visibility == ChatType.PROTECTED) || (!chatPatched && room.visibility == ChatType.PROTECTED)) && // if the room is protected, show the password field
 				<form method="post" onSubmit={submitPassWordChange}>
 					<input className="form-control form-control-sm" placeholder="new password" type="password" name="password" required={true} autoComplete="new-password" minLength={6} maxLength={30} onChange={(e) => setPassword(e.target.value)}></input>
-					<input type="submit" value="Save Password" />
+					{showSuccess ? <>password saved! <input type="button" value="change again" onClick={() => setShowSuccess(false)}/></> : <input type="submit" value="Save Password" />}
 				</form>
 			}
 			<br />&emsp;&emsp; [with set/change password option]<br />
 			<form onSubmit={submitChannelNameChange} acceptCharset='utf-8' className="row">
-				<EditableDataField name="Channel name" data={room.name}>
+				<EditableDataField name="Channel name" data={chatPatched? chatPatched.name : room.name} close={chatPatched ? true : false}>
 					<input className="form-control form-control-sm" placeholder={room.name} type="text" name="name" required={false} autoComplete="off" minLength={6} maxLength={30}></input> {/* //todo: JMA: finetune min and max */}
 				</EditableDataField>
 			</form>
