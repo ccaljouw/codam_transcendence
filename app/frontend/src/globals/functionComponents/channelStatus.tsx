d9d6f9fb90fb6578894a79e052ca021d8b420245
post@albertvanandel.nl <you@example.com>
2024-09-19 23:01:46 +0200

    redid channel status indicator

--- app/frontend/src/globals/functionComponents/channelStatus.tsx
+++ app/frontend/src/globals/functionComponents/channelStatus.tsx
@@ -1,7 +1,10 @@
 import { useContext, useEffect, useState } from "react";
 import { TranscendenceContext } from "../contextprovider.globalvar";
 import { OnlineStatus } from "@prisma/client";
-import { stat } from "fs";
+import useFetch from '@ft_global/functionComponents/useFetch';
+import { UpdateChatUserDto } from "@ft_dto/chat";
+import { constants } from "../constants.globalvar";
+
 
 const StatusDisplay = {
 	IN_CHANNEL: '🔵',
@@ -11,27 +14,41 @@ const StatusDisplay = {
 
 export default function ChannelStatusIndicator(props: { userId: number, onlineStatus: OnlineStatus }): JSX.Element {
 	const { currentChatRoom, someUserUpdatedTheirStatus } = useContext(TranscendenceContext);
+	const { data: chatUser, isLoading: chatUserIsLoading, error: chatUserError, fetcher: chatUserFetcher } = useFetch<null, UpdateChatUserDto>();
+	const [status, setStatus] = useState<string>(OnlineStatus.OFFLINE);
 
-	const statusSetter = () => {
-		if (!currentChatRoom || currentChatRoom.users === undefined) return StatusDisplay.OFFLINE;
-		const user = currentChatRoom.users.find(user => user.userId === props.userId);
-		if (!user) return StatusDisplay.OFFLINE;
-		if (user.isInChatRoom) 
-				console.log('user in channel', user);
-		else
-			console.log('user not in channel', user);
-		if (props.onlineStatus !== OnlineStatus.OFFLINE) {
-			return user.isInChatRoom ? StatusDisplay.IN_CHANNEL : StatusDisplay.ONLINE;
-		} else {
-			return StatusDisplay.OFFLINE;
-		}
-	}
+	// const statusSetter = () => {
+	// 	if (!currentChatRoom || currentChatRoom.users === undefined) return StatusDisplay.OFFLINE;
+	// 	const user = currentChatRoom.users.find(user => user.userId === props.userId);
+	// 	if (!user) return StatusDisplay.OFFLINE;
+	// 	if (user.isInChatRoom) 
+	// 			console.log('user in channel', user);
+	// 	else
+	// 		console.log('user not in channel', user);
+	// 	if (props.onlineStatus !== OnlineStatus.OFFLINE) {
+	// 		return user.isInChatRoom ? StatusDisplay.IN_CHANNEL : StatusDisplay.ONLINE;
+	// 	} else {
+	// 		return StatusDisplay.OFFLINE;
+	// 	}
+	// }
 
-	const [status, setStatus] = useState<string>(statusSetter());
 
 	useEffect(() => {
-		setStatus(statusSetter());
+		console.log("ChannelStatus: currentChatRoom UseEffect");
+		chatUserFetcher({url: constants.CHAT_GET_CHATUSER + currentChatRoom.id + '/' + props.userId})
+		// setStatus(statusSetter());
 	}, [currentChatRoom])
+
+
+	useEffect(() => {
+		console.log("ChannelStatus: chatUser UseEffect", chatUser);
+		if (chatUser === undefined || chatUser === null) return;
+		if (chatUser.isInChatRoom) {
+			setStatus(StatusDisplay.IN_CHANNEL);
+		} else {
+			setStatus(StatusDisplay.ONLINE);
+		}
+	}, [chatUser])
 
 	useEffect(() => {
 		console.log('someUserUpdatedTheirStatus', someUserUpdatedTheirStatus);
