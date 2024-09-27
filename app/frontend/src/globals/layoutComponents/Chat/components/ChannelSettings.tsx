263464d9f8f50f3a07f0ef4c215cf5f71acae5f0
post@albertvanandel.nl <you@example.com>
2024-09-27 22:27:14 +0200

    some steps to cleanup the channel settings

--- app/frontend/src/globals/layoutComponents/Chat/components/ChannelSettings.tsx
+++ app/frontend/src/globals/layoutComponents/Chat/components/ChannelSettings.tsx
@@ -54,16 +54,10 @@ export default function ChannelSettings({ room }: { room: FetchChatDto }) {
 		}
 	}, [chatPatched]);
 
-	const handleDeleteClick = async () => {
-		console.log("submitting form field entry");
-		await chatDeleter({ url: constants.API_CHAT + room.id, fetchMethod: 'DELETE' });
-	}
-
 	return (
 		<>
-			<b>Channel settings here.</b><br />
 			<form onChange={submitChannelNameChange} acceptCharset='utf-8' className="row">
-				<div className="col col-3">
+				<div className="col col-5">
 					<p>Channel visibility</p>
 				</div>
 				<div className="col col-6">
@@ -77,21 +71,15 @@ export default function ChannelSettings({ room }: { room: FetchChatDto }) {
 			</form>
 			{((chatPatched && chatPatched.visibility == ChatType.PROTECTED) || (!chatPatched && room.visibility == ChatType.PROTECTED)) && // if the room is protected, show the password field
 				<form method="post" onSubmit={submitPassWordChange}>
-					<input className="form-control form-control-sm" placeholder="new password" type="password" name="password" required={true} autoComplete="new-password" minLength={6} maxLength={30} onChange={(e) => setPassword(e.target.value)}></input>
+					<input className="form-control form-control-sm col col-6" placeholder="new password" type="password" name="password" required={true} autoComplete="new-password" minLength={6} maxLength={30} onChange={(e) => setPassword(e.target.value)}></input>
 					{showSuccess ? <>password saved! <input type="button" value="change again" onClick={() => setShowSuccess(false)}/></> : <input type="submit" value="Save Password" />}
 				</form>
 			}
-			<br />&emsp;&emsp; [with set/change password option]<br />
 			<form onSubmit={submitChannelNameChange} acceptCharset='utf-8' className="row">
 				<EditableDataField name="Channel name" data={chatPatched? chatPatched.name : room.name} close={chatPatched ? true : false}>
 					<input className="form-control form-control-sm" placeholder={room.name} type="text" name="name" required={false} autoComplete="off" minLength={6} maxLength={30}></input> {/* //todo: JMA: finetune min and max */}
 				</EditableDataField>
 			</form>
-			{/* todo: albert: trigger reload when chat patch worked */}
-			{/* {chatPatched && setCurrentChatRoom} */}
-			{patchIsLoading && "Updating chat name..."}
-			<button className="btn btn-outline-dark btn-sm" onClick={handleDeleteClick} >Delete channel</button>
-			{chatDeleted == true && "Albert: trigger page reload"}
 		</>
 	)
 }
