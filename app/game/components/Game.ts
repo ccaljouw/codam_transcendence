0efdd61c27703c263c36afba87f5be5e696f9881
Skipper <post@albertvanandel.nl>
2024-10-05 00:24:22 +0200

    gameloop change from carlo

--- app/game/components/Game.ts
+++ app/game/components/Game.ts
@@ -25,15 +25,15 @@ export class Game {
 	gameState: GameState = GameState.WAITING;
 	keyListener: KeyListenerComponent = new KeyListenerComponent();
 	soundFX: SoundFX = new SoundFX();
-	gameUsers: UpdateGameUserDto [] = [];
-	walls: Wall [] = [] ;
-	lines: GameObject [] = [];
-	paddels: Paddle [] = [];
-	players: PlayerComponent [] = [];
+	gameUsers: UpdateGameUserDto[] = [];
+	walls: Wall[] = [];
+	lines: GameObject[] = [];
+	paddels: Paddle[] = [];
+	players: PlayerComponent[] = [];
 	ball: Ball | null = null;
-	messageFields: TextComponent [] = [];
+	messageFields: TextComponent[] = [];
 	backgroundFill: GameObject | null = null;
-	receivedUpdatedGameObjects: UpdateGameObjectsDto = {roomId: -1, ballX: -1, ballY: -1, ballDirection: -1, ballSpeed: 0, ballDX: -1, ballDY: -1, paddle1Y: -1, paddle2Y: -1, score1: -1, score2: -1, resetGame: -1, resetMatch: -1, winner: -1};
+	receivedUpdatedGameObjects: UpdateGameObjectsDto = { roomId: -1, ballX: -1, ballY: -1, ballDirection: -1, ballSpeed: 0, ballDX: -1, ballDY: -1, paddle1Y: -1, paddle2Y: -1, score1: -1, score2: -1, resetGame: -1, resetMatch: -1, winner: -1 };
 	winner: PlayerComponent | null = null;
 	roomId: number = 0;
 	elapasedTimeSincceLastUpdate: number = 0;
@@ -50,9 +50,9 @@ export class Game {
 		this.gameData = data;
 		this.instanceType = instanceType;
 		this.roomId = this.gameData.id;
-		this.canvas = newCanvas? newCanvas : undefined;
+		this.canvas = newCanvas ? newCanvas : undefined;
 		this.ctx = this.canvas?.getContext("2d") as CanvasRenderingContext2D;
-		this.gameUsers = this.gameData.GameUsers as UpdateGameUserDto [];
+		this.gameUsers = this.gameData.GameUsers as UpdateGameUserDto[];
 		this.soundFX.setVolume(givenConfig, givenVolume);
 		this.aiLevel = aiLevel;
 		this.firstBike = context.firstBike.value;
@@ -67,38 +67,35 @@ export class Game {
 		context.subscribeToBikeUpdates(this.updateBikeValues.bind(this))
 		log(`GameScript: instance type: ${this.instanceType}`);
 	}
-	
+
 	updateBikeValues(firstBike: number, secondBike: number) {
-        this.firstBike = firstBike;
-        this.secondBike = secondBike;
-    }
+		this.firstBike = firstBike;
+		this.secondBike = secondBike;
+	}
 
 	redrawGameObjects() {
 		this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 		drawGameObjects(this);
 	}
 
-	gameLoop(currentTime:number) {
+	gameLoop(currentTime: number) {
 		let deltaTime = (currentTime - this.lastFrameTime) / 1000;
 		this.lastFrameTime = currentTime;
-		
-		if (this.gameState == GameState.FINISHED ) {
+		if (this.gameState == GameState.FINISHED) {
+			this.soundFX.stopAll();
 			return;
 		}
-
 		if (this.gameState == GameState.WAITING && this.canvas) {
 			this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 			this.messageFields[0].setText(CON.config[this.config].startMessage);
 		}
-		
 		if (this.gameState == GameState.STARTED) {
 			updateObjects(this, deltaTime);
 			checkForGoals(this);
 		}
-		
 		if (this.canvas) {
-				this.redrawGameObjects();
-				this.currentAnimationFrame = requestAnimationFrame(this.gameLoop.bind(this));
+			this.redrawGameObjects();
+			this.currentAnimationFrame = requestAnimationFrame(this.gameLoop.bind(this));
 		}
 	}
 
@@ -106,13 +103,13 @@ export class Game {
 		if (this.gameState === GameState.FINISHED) {
 			return;
 		}
-		
+
 		if (winningSide === this.instanceType) {
 			this.soundFX.playWin();
 		} else {
 			this.soundFX.playLose();
 		}
-		
+
 		this.gameState = GameState.FINISHED;
 		this.winner = this.players[winningSide];
 		if (this.canvas) {
@@ -156,7 +153,7 @@ export class Game {
 		countdown(this);
 		this.soundFX.playGoal();
 	}
- 
+
 	startGame() {
 		this.gameState = GameState.STARTED;
 		countdown(this);
@@ -185,7 +182,7 @@ export class Game {
 		this.backgroundFill = null;
 		this.gameData = null;
 		this.gameUsers = [];
-		this.receivedUpdatedGameObjects = {roomId: -1, ballX: -1, ballY: -1, ballDirection: -1, ballSpeed: 0, ballDX: -1, ballDY: -1, paddle1Y: -1, paddle2Y: -1, score1: -1, score2: -1, resetGame: -1, resetMatch: -1, winner: -1};
+		this.receivedUpdatedGameObjects = { roomId: -1, ballX: -1, ballY: -1, ballDirection: -1, ballSpeed: 0, ballDX: -1, ballDY: -1, paddle1Y: -1, paddle2Y: -1, score1: -1, score2: -1, resetGame: -1, resetMatch: -1, winner: -1 };
 		this.winner = null;
 		this.roomId = 0;
 		this.elapasedTimeSincceLastUpdate = 0;
